8e8e0b42a37fbf81c0fefb934ea6315506b3aea0
Binod Raj Pandey <binod@fusemachines.com>
2018-01-08 17:39:01 +0545

    intial setup

---
+++ src/main/java/com/example/demo/entities/Department.java
@@ -0,0 +1,57 @@
+
+package com.example.demo.entities;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author binod
+ */
+@Entity
+public class Department {
+    @Id
+    @GeneratedValue
+    @Column
+    private Long departmentId;
+    @Column(unique = true)
+    private String departmentName;
+    
+    @JsonManagedReference
+@OneToMany(mappedBy = "department",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    List<Student> studentList=new ArrayList<>();
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+  
+    
+}
